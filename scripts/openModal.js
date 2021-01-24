fb516d456b01329125bf93775adb78c5f6822628
xSallus <souza95salomao@gmail.com>
2021-01-24 14:26:35 -0400

    refactor: stylesheets

--- scripts/openModal.js
+++ scripts/openModal.js
@@ -10,5 +10,18 @@ const Modal = {
             .querySelector('.modal-overlay')
             .classList
             .remove('active')
+    },
+    delete() {
+        //lmao
+    },
+    changeTheme() {
+        //change theme -> rocket-seat
+        // .cabecalho -> rocket-seat-dark;
+        // .cards.total -> rocket-seat;
+        // body -> dark-blue;
+        // save button -> my-purple;
+        // cancel button -> my-purple;
+        // add transaction button ->  rocket-seat-dark;
+        // add transaction button:hover -> rocket-seat;
     }
 }
