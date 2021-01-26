8c4c5656c7208706d07a5fd92feccd5938c27dc4
xSallus <souza95salomao@gmail.com>
2021-01-26 10:31:50 -0400

    Some minnor updates

---
+++ scripts/themes.js
@@ -0,0 +1,11 @@
+/*const  themeDark
+const themeDefault
+const themeRocketseat*/
+
+function changeTheme() {
+    let body = document.body;
+    let cards = document.querySelectorAll('.cards');
+    let toogle = document.querySelector('.toogle');
+
+    toogle.style.background = "#F00";
+}
