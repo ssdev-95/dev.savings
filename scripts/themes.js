f4ac3b61e427dea242d9ed9b2a109efe3dbd2de4
xSallus <souza95salomao@gmail.com>
2021-01-27 11:06:29 -0400

    ADD[feature]: dark theme alongside with rocketseat theme

--- scripts/themes.js
+++ scripts/themes.js
@@ -1,11 +1,73 @@
-/*const  themeDark
-const themeDefault
-const themeRocketseat*/
+const  themeDark = [/*.some texts*/'#fff',
+                    /*header*/'#000',
+                    /*body*/'#484d50',
+                    /*cards*/'#235a60',
+                    /*cardTotal*/'#40e0d0',
+                    /*toogle*/'#0037a4',
+                    /*footer*/'#fff']
 
-function changeTheme() {
-    let body = document.body;
-    let cards = document.querySelectorAll('.cards');
-    let toogle = document.querySelector('.toogle');
+const themeDefault = [/*.some texts*/'#000',
+                      /*header*/'#12a454',
+                      /*body*/'#f0f2f5',
+                      /*cards*/'#fff',
+                      /*cardTotal*/' #3dd705',
+                      /*toogle*/'#000',
+                      /*footer*/'#363f5f']
 
-    toogle.style.background = "#F00";
+const themeRocketseat = [/*.some texts*/'#fff',
+                         /*header*/'#7159c1',
+                         /*body*/'#000',
+                         /*cards*/'#9370DB',
+                         /*cardTotal*/'#8A2BE2',
+                         /*toogle*/'#fe6e00',
+                         /*footer*/'#fff']
+
+let style = 'light'
+
+let head = document.querySelector('.cabecalho')
+let body = document.body
+let cards = document.querySelectorAll('.cards')
+let cardTotal = document.querySelector('.cards.total')
+let toogle = document.querySelector('.circle')
+let foot = document.querySelector('.rodape')
+let tableDate = document.querySelectorAll('.date')
+let tableDescription = document.querySelectorAll('.description')
+
+function selectStyle() {
+    switch (style) {
+        case 'light':
+            changeColor(themeDark);
+            style = 'dark'
+            break;
+        case 'dark':
+            changeColor(themeRocketseat)
+            style = 'rocket'
+            break;
+        case 'rocket':
+            changeColor(themeDefault)
+            style = 'light'
+            break;
+    }
+}
+
+function changeColor(pallette) {
+    head.style.background = pallette[1]
+    body.style.background = pallette[2]
+    cardTotal.style.background = pallette[4]
+    toogle.style.fill = pallette[5]
+
+    for(i=0; i<2; i++) {
+        cards.item(i).style.background = pallette[3]
+        cards.item(i).style.color = pallette[0]
+    }
+
+    for (x=0; x<tableDate.length; x++) {
+        tableDate[x].style.color = pallette[6]
+        tableDescription[x].style.color = pallette[6]
+    }
+
+    //tableExpense.style.color = pallette[6]
+    //tableIncome.style.color = pallette[6]
+
+    foot.style.color = pallette[6]
 }
