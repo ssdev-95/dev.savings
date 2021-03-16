0d3a14ef8ca386aac6d532380430c4f0f213596c
Salomao Souza <souza95salomao@gmail.com>
2021-03-16 12:22:08 -0400

    [REFACTOR] Moving project from HTML/CSS/JS to Next.js

---
+++ src/pages/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import styles from '../styles/pages/Home.module.css'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+    </div>
+  )
+}
