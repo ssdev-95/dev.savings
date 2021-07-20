2f4528fc1ebd3fda4566fb621018d9d47f39f15b
Salomao Souza <souza95salomao@gmail.com>
2021-07-20 13:18:37 -0400

    [UPDATE] Routes especifications and route mismatches

--- src/routes.ts
+++ src/routes.ts
@@ -1,16 +1,31 @@
-import { Router } from 'express'
+import { Request, Response, Router } from 'express'
+
 import { AddTransactionController } from './controllers/AddTransactionController'
 import { DeleteTransactionController } from './controllers/DeleteTransactionController'
+import { UpdateTransactionController } from './controllers/UpdateTransactionController'
+import { RetrieveTransactionsController } from './controllers/RetrieveTransactionsController'
 
 const addTransactionController = new AddTransactionController()
 const deleteTransactionController = new DeleteTransactionController()
+const updateTransactionController = new UpdateTransactionController()
+const retrieveTransactionsController = new RetrieveTransactionsController()
+
+async function handle404Redirect(req:Request, res:Response) {
+    return res.json({ msg: 'Feature not found' })
+}
 
 const router = Router()
 
-router.get('/users/:id/transactions', (req, res)=>res.json({ msg: `all user(${req.params['id']}) transactions will appear here` }))
+router.get('/users/:id/transactions/:slug', handle404Redirect)
+
+router.get('/users/:id/transactions/:slug/update', handle404Redirect)
 
 router.post('/users/:id/transactions', addTransactionController.handle)
 
-router.post('/users/:id/transactions/:slug', deleteTransactionController.handle)
+router.get('/users/:id/transactions', retrieveTransactionsController.handle)
 
-export  { router }
+router.delete('/users/:id/transactions/:slug', deleteTransactionController.handle)
+
+router.post('/users/:id/transactions/:slug', updateTransactionController.handle)
+
+export { router }
