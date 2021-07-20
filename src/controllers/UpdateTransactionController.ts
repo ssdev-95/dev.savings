b88365b377c01559bd0277c45d5d1a17bcb7f8ee
Salomao Souza <souza95salomao@gmail.com>
2021-07-20 13:19:56 -0400

    [UPDATE] Update product feature

--- src/controllers/UpdateTransactionController.ts
+++ src/controllers/UpdateTransactionController.ts
@@ -1,9 +1,33 @@
 import { Request, Response} from 'express'
+import { Transaction } from '../@types'
 import {UpdateTransactionService } from '../services/UpdateTransactionService'
 
 class UpdateTransactionController {
 
-    async handle(req:Request, res:Response) {}
+    async handle(req:Request, res:Response) {
+        const {
+            description,
+            amount,
+            when,
+            category,
+            owner
+        } = req.body as Transaction
+
+        const id = req.params['slug']
+
+        const updateTransactionService = new UpdateTransactionService()
+        
+        const hasUpdated = await updateTransactionService.execute({
+            id: id,
+            description: description,
+            amount: amount,
+            category: category,
+            when: when,
+            owner: owner
+        })
+
+        return res.json({ hasUpdated: hasUpdated })
+    }
 }
 
 export { UpdateTransactionController }
--- src/services/UpdateTransactionService.ts
+++ src/services/UpdateTransactionService.ts
@@ -1,5 +1,24 @@
+import { database } from '../db/firebase'
+import { Transaction } from '../@types'
+
 class UpdateTransactionService {
-    async execute() {}
+
+    async execute(transaction: Transaction) {
+        try {
+            await database.collection('transactions').doc(transaction.id).set({
+                description: transaction.description,
+                amount: transaction.amount,
+                when: transaction.when,
+                category: transaction.category,
+                owner: transaction.owner
+            })
+
+            return true
+        } catch(err) {
+            console.error(err)
+            return false
+        }
+    }
 }
 
 export { UpdateTransactionService }
