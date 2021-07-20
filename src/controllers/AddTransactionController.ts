6241fc90c277eed768976b999dbf38db4bf27f31
Salomao Souza <souza95salomao@gmail.com>
2021-07-20 13:19:18 -0400

    [UPDATE] Add product feature

--- src/controllers/AddTransactionController.ts
+++ src/controllers/AddTransactionController.ts
@@ -1,16 +1,18 @@
 import { Request, Response } from 'express'
+import { Transaction } from '../@types'
 import { AddTransactionService } from '../services/AddTransactionService'
 
 class AddTransactionController  {
     async handle(req:Request, res:Response) {
-        const { description, amount, when } = req.body
+        const { description, amount, when, category } = req.body as Transaction
         const id = req.params['id']
 
         const addTransactionService = new AddTransactionService()
 
-        const record = addTransactionService.execute({
+        const record = await addTransactionService.execute({
             description: description,
             amount: amount,
+            category: category,
             when: when,
             owner: id
         })
--- src/services/AddTransactionService.ts
+++ src/services/AddTransactionService.ts
@@ -3,16 +3,28 @@ import { Transaction } from '../@types'
 
 class AddTransactionService {
 
-    async execute({description, amount, when, owner }: Transaction) {
-        const transity = {
+    async execute({description, amount, when, owner, category }: Transaction) {
+        const transit = {
             description: description,
             amount: amount,
             when: when,
-            owner: owner
+            owner: owner,
+            category: category
         }
 
-        const result = await database.collection('transactions').add(transity)
-        return result
+        const result = await database.collection('transactions').add(transit)
+        const record =  await result.get()
+        
+        const newTransaction = {
+            id: record.id,
+            description: record.data().description,
+            amount: record.data().amount,
+            category: record.data().category,
+            when: record.data().when,
+            owner: record.data().owner
+        } as Transaction
+
+        return newTransaction
     }
 }
 
