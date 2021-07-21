19b5587358c14c9ec75593ede3636f3b05b4f820
Salomao Souza <souza95salomao@gmail.com>
2021-07-21 14:07:38 -0400

    [UPDATE] Routes

--- src/routes.ts
+++ src/routes.ts
@@ -8,6 +8,9 @@ import { UpdateTransactionController } from './controllers/transactions/UpdateTr
 import { RetrieveTransactionsController } from './controllers/transactions/RetrieveTransactionsController'
 
 import { CreateUserController } from './controllers/users/CreateUserController'
+import { UpdateUserController } from './controllers/users/UpdateUserController'
+import { DeleteUserController } from './controllers/users/DeleteUserController'
+import { RetrieveUserController } from './controllers/users/RetrieveUserController'
 
 const addTransactionController = new AddTransactionController()
 const deleteTransactionController = new DeleteTransactionController()
@@ -15,6 +18,9 @@ const updateTransactionController = new UpdateTransactionController()
 const retrieveTransactionsController = new RetrieveTransactionsController()
 
 const createUserController = new CreateUserController()
+const updateUserController = new UpdateUserController()
+const deleteUserController = new DeleteUserController()
+const retrieveUserController = new RetrieveUserController()
 
 async function handle404Redirect(req:Request, res:Response) {
     return res.status(404).json({ status: 404, msg: 'Feature not found' })
@@ -29,9 +35,9 @@ router.delete('/users/:id/transactions/:slug', deleteTransactionController.handl
 // router.get('/users/:id/transactions/:slug/update', handle404Redirect)
 
 // ############ User Routes ############
-router.get('/users', handle404Redirect)
+router.get('/users', retrieveUserController.handle)
 router.post('/users', createUserController.handle)
-router.delete('/users', handle404Redirect)
-router.patch('/users', handle404Redirect)
+router.delete('/users', deleteUserController.handle)
+router.put('/users', updateUserController.handle)
 
 export { router }
