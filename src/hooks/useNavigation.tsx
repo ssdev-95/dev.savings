4eb2ccb6029c30b271b83b6be399c444fed4a28b
Salomao Souza <souza95salomao@gmail.com>
2021-07-27 11:44:47 -0400

    [REMOVE] Unused files

--- src/hooks/useNavigation.tsx
+++
@@ -1,9 +0,0 @@
-import { useContext } from 'react'
-import { NavigationContext } from 'src/contexts/Navigation'
-
-function useNavigation() {
-
-    return useContext(NavigationContext)
-}
-
-export { useNavigation }
