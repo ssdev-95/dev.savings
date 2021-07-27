011637e0e2bf14b59c3231bc5fe96ef6791eacd3
Salomao Souza <souza95salomao@gmail.com>
2021-07-27 15:20:22 -0400

    [UPDATE] Home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,9 +1,23 @@
+import { useHistory } from 'react-router-dom'
+import { FaArrowRight/*, FaTrashAlt*/ } from 'react-icons/fa'
+import ILogo from 'src/icons/logo.svg'
 import styles from './styles.module.scss'
 
 function App() {
+  const router = useHistory()
 
   return (
-    <div></div>
+    <main className={styles.home}>
+      <img src={ILogo} alt="Dev.$avings" />
+      <section>
+        <h2>Studies shows that</h2>
+        <p>you'll always have money if..</p>
+      </section>
+      <button onClick={()=>router.push('/auth0')}>
+        <span>Start saving today</span>
+        <FaArrowRight />
+      </button>
+    </main>
   )
 }
 
