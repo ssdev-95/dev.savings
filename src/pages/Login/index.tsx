d0154f193c02e15be9c0482422048dff16f9e848
Salomao Souza <souza95salomao@gmail.com>
2021-07-29 13:58:09 -0400

    [UPDATE] Auth page/feature feature

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,10 +1,84 @@
+import { useState, ChangeEvent, FormEvent } from 'react'
+import { useHistory } from 'react-router-dom'
+import { useAuth } from 'src/hooks/useAuth'
+import ILogo from 'src/icons/logo.svg'
 import styles from './styles.module.scss'
 
 function LoginPage() {
+    const history = useHistory()
+    const {
+        signUpWithEmailAndPassword,
+        loginWithEmailAndPassword ,
+        loginWithGoogle,
+        loginWithGithub
+    } = useAuth()
+
+    const [credentials, setCredentials] = useState({
+        email: '',
+        password: ''
+    })
+
+    function handleChange(event: ChangeEvent<HTMLInputElement>) {
+        event.preventDefault()
+
+        const value = event.currentTarget.value
+        const ref = event.currentTarget.name
+
+        setCredentials({
+            ...credentials,
+            [ref]: value
+        })
+    }
+
+    async function handleSubmitLogin(event: FormEvent) {
+        event.preventDefault()
+
+        const { email, password } = credentials
+
+        await loginWithEmailAndPassword(email, password)
+    }
+
+    async function handleSubmitSignUp() {
+        const { email, password } = credentials
+
+        await signUpWithEmailAndPassword(email, password)
+    }
 
     return (
-        <main className={styles.login}>
-            <span>Login page</span>
+        <main className={styles.container}>
+            <header className={styles.navbar}>
+                <img
+                  src={ILogo}
+                  alt="Dev.$avings"
+                  onClick={()=>history.push('/')}
+                />
+            </header>
+            <section className={styles['login-left']}>
+                <form
+                  name="credentials"
+                  id="credentials"
+                  onSubmit={handleSubmitLogin}
+                >
+                    <input
+                      name="email"
+                      type="email"
+                      required
+                      onChange={handleChange}
+                    />
+                    <input
+                      name="password"
+                      type="password"
+                      required
+                      onChange={handleChange}
+                    />
+                </form>
+                <button type="submit" form="credentials">Sign In</button>
+            </section>
+            <aside className={styles['login-right']}>
+                <button onClick={handleSubmitSignUp}>Sign Up</button>
+                <button onClick={loginWithGoogle}>Save with Google</button>
+                <button onClick={loginWithGithub}>Save with Github</button>
+            </aside>
         </main>
     )
 }
