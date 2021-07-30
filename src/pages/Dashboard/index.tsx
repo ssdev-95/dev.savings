1d2b0bd677d23fc869c2a4cf59dac7a591fe4326
Salomao Souza <souza95salomao@gmail.com>
2021-07-30 16:26:59 -0400

    [UPDATE] Dashboard styles and content

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,10 +1,34 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { Pie } from 'react-chartjs-2'
+import { useTransactions } from 'src/hooks/useTransactions'
+
+import { Table } from 'src/components/Table'
+import ILogo from 'src/icons/logo.svg'
+import ICoin from 'src/icons/coin.png'
+
 import styles from './styles.module.scss'
 
 function Dash() {
+    const { transactions, incomes, expenses } = useTransactions()
 
     return (
         <main className={styles.dash}>
-            <span>Dashboard</span>
+            <section>
+                <div>
+                    <img src={ILogo} alt="dev.$avings" />
+                    <span>value</span>
+                    <button>add</button>
+                </div>
+                {/* <img className={styles.chart} src={ICoin} alt="Dogecoin"/> */}
+                {/* Finalize tomorrow :D */}
+                <Pie
+                  redraw
+                  data={[ ['incomes', incomes], ['expenses', expenses] ]}
+                />
+            </section>
+            <section>
+                <Table transactions={transactions} />
+            </section>
         </main>
     )
 }
